6c80bb8bcbcfbc26711c174f7fd42631d930366d
martha <martha.locks@alibin.com.br>
2022-03-05 10:37:20 -0300

    First commit.

---
+++ app/Services/Author/AuthorService.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services\Author;
+
+use App\Services\AbstractService;
+
+/**
+ * class AuthorService
+ * @package App\Services\AuthorService
+ */
+class AuthorService extends AbstractService
+{
+}
---
+++ routes/web.php
@@ -0,0 +1,97 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+//http:localhost:8080/api/v1
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\Author'], function () use ($router) {
+    $router->post('/authors', [
+        'uses' => 'AuthorController@create'
+    ]);
+    $router->get('/authors', [
+        'uses' => 'AuthorController@findAll'
+    ]);
+    $router->get('/authors/{id}', [
+        'uses' => 'AuthorController@findOneBy'
+    ]);
+    $router->put('/authors/{param}', [
+        'uses' => 'AuthorController@editBy'
+    ]);
+    $router->patch('/authors/{param}', [
+        'uses' => 'AuthorController@editBy'
+    ]);
+    $router->delete('/authors/{id}', [
+        'uses' => 'AuthorController@delete'
+    ]);
+});
+
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\News'], function () use ($router) {
+    $router->post('/news', [
+        'uses' => 'NewsController@create'
+    ]);
+    $router->get('/news', [
+        'uses' => 'NewsController@findAll'
+    ]);
+    $router->get('/news/author/{author}', [
+        'uses' => 'NewsController@findByAuthor'
+    ]);
+    $router->get('/news/{param}', [
+        'uses' => 'NewsController@findBy'
+    ]);
+    $router->put('/news/{param}', [
+        'uses' => 'NewsController@editBy'
+    ]);
+    $router->patch('/news/{param}', [
+        'uses' => 'NewsController@editBy'
+    ]);
+    $router->delete('/news/{param}', [
+        'uses' => 'NewsController@deleteBy'
+    ]);
+    $router->delete('/news/{author}', [
+        'uses' => 'NewsController@deleteByAuthor'
+    ]);
+});
+
+$router->group(['prefix' => 'api/v1', 'namespace' => 'V1\ImageNews'], function () use ($router) {
+    $router->post('/imageNews', [
+        'uses' => 'ImageNewsController@create'
+    ]);
+    $router->get('/imageNews', [
+        'uses' => 'ImageNewsController@findAll'
+    ]);
+    $router->get('/imageNews/news/{news}', [
+        'uses' => 'ImageNewsController@findByNews'
+    ]);
+    $router->get('/imagens-noticias/{id}', [
+        'uses' => 'ImageNewsController@findOneBy'
+    ]);
+    $router->get('/imageNews/{param}', [
+        'uses' => 'ImageNewsController@findBy'
+    ]);
+    $router->put('/imageNews/{param}', [
+        'uses' => 'ImageNewsController@editBy'
+    ]);
+    $router->patch('/imageNews/{param}', [
+        'uses' => 'ImageNewsController@editBy'
+    ]);
+    $router->delete('/imageNews/news/{news}', [
+        'uses' => 'ImageNewsController@deleteByNews'
+    ]);
+    $router->delete('/imageNews/{id}', [
+        'uses' => 'ImageNewsController@delete'
+    ]);
+});
